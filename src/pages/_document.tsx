d95577688bcb15b531379f40f49bdfeaffb9960f
Po YZ <poyizhi@gmail.com>
2023-12-30 10:58:42 +0800

    bug: put speedInsights at _app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-// lib
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Document() {
   return (
@@ -9,7 +7,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <SpeedInsights />
       </body>
     </Html>
   );
