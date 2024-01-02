1435fdc81e67bb60bc2567919ecfa8af1d3239ee
Po YZ <poyizhi@gmail.com>
2024-01-02 15:15:12 +0800

    style: add Raleway and Montserrat font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import "@/styles/globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { AppProps } from "next/app";
+// fonts
+import { Montserrat, Raleway } from "next/font/google";
+// vercel
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  variable: "--font-raleway",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-monts",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={`${raleway.variable} ${montserrat.variable}`}>
       <Component {...pageProps} />
       <SpeedInsights />
-    </>
+    </div>
   );
 }
